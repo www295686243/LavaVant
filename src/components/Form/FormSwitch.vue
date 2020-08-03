f2f4d5c064381db08554bb67d34f8041dd4a3911
wanx <wanx@app.com>
2020-08-03 14:01:04 +0800

    优化switch组件

--- src/components/Form/FormSwitch.vue
+++ src/components/Form/FormSwitch.vue
@@ -3,59 +3,23 @@
     class="FormSwitch"
     :label="field.label">
     <template #input>
-      <van-switch
-        v-model="innerValue"
-        @change="onChange"
-        active-color="#07c160"
-        :active-value="1"
-        :inactive-value="0"
-        :loading="isLoading"
-        :size="size"
-        v-bind="$attrs"/>
+      <SwitchBox v-model="innerValue"></SwitchBox>
     </template>
   </van-field>
 </template>
 
 <script lang="ts">
 import FormMixins from './FormMixins'
-import { Component, Mixins, Prop } from 'vue-property-decorator'
-import { Field, Switch } from 'vant'
-import VantService from '@/service/VantService'
-import { PromiseResult } from '@/plugins/axios'
+import { Component, Mixins } from 'vue-property-decorator'
+import { Field } from 'vant'
+import SwitchBox from './Base/SwitchBox.vue'
 
 @Component({
-  inheritAttrs: false,
   components: {
     [Field.name]: Field,
-    [Switch.name]: Switch
+    SwitchBox
   }
 })
 export default class FormSwitch extends Mixins(FormMixins) {
-  @Prop()
-  onSubmit!: Function
-
-  @Prop({ default: 24 })
-  size!: number
-
-  private isLoading = false
-
-  private onChange () {
-    if (this.onSubmit) {
-      this.isLoading = true
-      this.onSubmit()
-        .then((res: PromiseResult) => {
-          this.isLoading = false
-          if (res && res.message) {
-            VantService.toast.success(res.message)
-          }
-        })
-        .catch((res: PromiseResult) => {
-          this.isLoading = false
-          if (res && res.message) {
-            VantService.toast.fail(res.message)
-          }
-        })
-    }
-  }
 }
 </script>
