1bfa0bbe1ba34daa3b1ccc896d1192a24f461bab
wanx <wanx@app.com>
2020-12-17 20:31:20 +0800

    小修复

--- src/abstract/HrAbstract.ts
+++ src/abstract/HrAbstract.ts
@@ -7,7 +7,6 @@ export default abstract class HrAbstract extends BaseAbstract {
   abstract index(params: { page: number }): Promise<PromiseResult>
   abstract show(): Promise<PromiseResult>
   abstract pay(params: { user_coupon_id: string }): Promise<{ message: string }>
-  abstract getContacts(): Promise<PromiseResult>
   abstract view(params: { id: string; su: string }): Promise<PromiseResult>
   abstract complaint(params: { complaint_type: string; complaint_content: string }): Promise<PromiseResult>
   abstract isComplaint(): Promise<PromiseResult>
